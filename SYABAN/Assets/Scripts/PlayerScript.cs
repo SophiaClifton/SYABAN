1c64080e41d365c50fb951fc73ed6d3627934c57
SophiaClifton <sophiashoco@gmail.com>
2023-11-04 17:58:42 -0400

    Added player stamina(lose and regain stamina over time)
    Added player stamina(lose and regain stamina over time)

--- SYABAN/Assets/Scripts/PlayerScript.cs
+++ SYABAN/Assets/Scripts/PlayerScript.cs
@@ -7,20 +7,25 @@ public class PlayerScript : MonoBehaviour
 {
     public Image HP; 
     public float HPWidth;
+    public Image staminaIMG; 
+    public float staminaWidth;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed = 8f;
     [SerializeField] private float jumpingPower = 16f;
     [SerializeField] private LayerMask blockingLayer;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float health;
+    [SerializeField] private float stamina;
     private float horizontal;
     private bool isFacingRight = true;
     public Animator animator;
+    private bool hasStamina = true;//use for determining if player can shoot /hit
 
     // Start is called before the first frame update
     void Start()
     {
         HPWidth = HP.rectTransform.rect.width;
+        staminaWidth = staminaIMG.rectTransform.rect.width;
     }
 
     // Update is called once per frame
@@ -73,6 +78,52 @@ public class PlayerScript : MonoBehaviour
             HPWidth -= amount;
             Vector2 temp = new Vector2(HPWidth, HP.rectTransform.rect.height);
             HP.rectTransform.sizeDelta = temp;
+        }
+    }
+
+    public void loseStamina()
+    {
+        
+        if(stamina>0)
+        {
+            stamina-=100;
+            staminaWidth -= 100;
+            Vector2 temp = new Vector2(staminaWidth, staminaIMG.rectTransform.rect.height);
+            staminaIMG.rectTransform.sizeDelta = temp;
+        }
+        else
+        {
+            hasStamina = false;
+        }
+    }
+
+    public void RegainStamina()
+    {
+        if(stamina+75 >=100)
+        {
+            stamina = 100;
+            staminaWidth = 100;
+        }
+        else
+        {
+            stamina += 75;
+            staminaWidth += 75;
+            Vector2 temp = new Vector2(staminaWidth, staminaIMG.rectTransform.rect.height);
+            staminaIMG.rectTransform.sizeDelta = temp;
+
+            if(stamina >= 100)
+            {
+                hasStamina = true;
+            }
+        }
+    }
+
+    IEnumerator RegainStaminaOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f); // Wait 1 sec
+            RegainStamina(); // Increase stamina by 75
         }
     }
 
