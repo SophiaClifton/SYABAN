8352aa8d1a65200fbe9605d2792b280ffaa3bc57
Alina-Alen <alina.alen.2003@gmail.com>
2023-11-05 01:57:39 -0500

    Added jumping animation

--- SYABAN/Assets/Scripts/PlayerScript.cs
+++ SYABAN/Assets/Scripts/PlayerScript.cs
@@ -34,7 +34,7 @@ public class PlayerScript : MonoBehaviour
     private bool hasStamina = true;//use for determining if player can shoot /hit
 
     public bool coroutineStarted;
-
+    public bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
