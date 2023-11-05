e69449bb522abd366f0cb459b1217fa1090e2da2
Bochinator <aidanboch@gmail.com>
2023-11-05 10:36:31 -0500

    Improved the pressure plate so it must be stood on. Boxes can be thrown around and rotated. Doors will SmoothDamp to their active pos and move back when deactivated.

--- SYABAN/Assets/Interactables/PressurePlate.cs
+++ SYABAN/Assets/Interactables/PressurePlate.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PressurePlate : Interactable
 {
-    
+    // Number of valid gameobjects triggering the pressure plate.
+    public float triggerCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,14 +26,22 @@ public class PressurePlate : Interactable
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.GetComponent<Box>() != null) {
+        if (other.GetComponent<Box>() != null || other.GetComponent<PlayerScript>() != null) {
+            triggerCount ++;
+        }
+        // Activate the linked obstacle on the first trigger.
+        if (triggerCount == 1) {
             spriteRenderer.sprite = activeSprite;
             linkedObstacle.Activate();
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if (other.GetComponent<Box>() != null) {
+        if (other.GetComponent<Box>() != null || other.GetComponent<PlayerScript>() != null) {
+            triggerCount --;
+        }
+        // If nothing is triggering the pressure plate, deactive the linked obstacle.
+        if (triggerCount == 0) {
             spriteRenderer.sprite = inactiveSprite;
             linkedObstacle.Deactivate();
         }
--- SYABAN/Assets/Obstacles/Door.cs
+++ SYABAN/Assets/Obstacles/Door.cs
@@ -4,27 +4,59 @@ using UnityEngine;
 
 public class Door : Obstacle
 {
+    [SerializeField] float activeXIncrease;
+    [SerializeField] float activeYIncrease;
+    [SerializeField] float openTime = 1;
     protected BoxCollider2D doorCollider;
     protected SpriteRenderer doorRenderer;
+
+    Vector3 velocity = Vector3.zero;
+    Vector3 inactivePos;
+    Vector3 activePos;
+
+    bool active = false;
+    bool reachedDestination = true;
     
     // Start is called before the first frame update
     void Start()
     {
         doorCollider = GetComponent<BoxCollider2D>();
         doorRenderer = GetComponent<SpriteRenderer>();
+        inactivePos = transform.position;
+        activePos = new Vector3(transform.position.x + activeXIncrease, transform.position.y + activeYIncrease, transform.position.z);
+    }
+
+    private void Update() {
+        if (!reachedDestination) {
+            if (active) {
+                transform.position = Vector3.SmoothDamp(transform.position, activePos, ref velocity, openTime);
+                if (Vector3.Distance(transform.position, activePos) <= 0.05) {
+                    transform.position = activePos;
+                    reachedDestination = true;
+                }
+            } else {
+                transform.position = Vector3.SmoothDamp(transform.position, inactivePos, ref velocity, openTime);
+                if (Vector3.Distance(transform.position, inactivePos) <= 0.05) {
+                    transform.position = inactivePos;
+                    reachedDestination = true;
+                }
+            }
+        }
     }
 
     // Allow the players to pass.
     public override void Activate() {
         Debug.Log("Activated");
-        doorCollider.enabled = false;
-        doorRenderer.enabled = false;
+
+        active = true;
+        reachedDestination = false;
     }
 
     // Block the players.
     public override void Deactivate() {
         Debug.Log("Deactivated");
-        doorCollider.enabled = true;
-        doorRenderer.enabled = true;
+
+        active = false;
+        reachedDestination = false;
     }
 }
